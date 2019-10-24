e2d4ff787a1db17ab96fd38def4534a0c7f9c0f0
tnentwig <tristan.nentwig@googlemail.com>
2019-10-24 23:32:07 +0200

    Reorganized parsing and intergation of variables

---
+++ src/pars/Elements/Simpleshape/Features/eventParser.ts
@@ -0,0 +1,46 @@
+import * as $ from 'jquery';
+
+// This function is parsing all <expr-...> tags like toggle color and returns a map with the expression as key and the variable as value
+
+export function parseDynamicParameters(section : JQuery<XMLDocument>) : Map<string, string> {
+    let exprMap : Map<string,string>= new Map();
+    let tags : Array<string>= [];
+    // Styling tags
+    tags.push("expr-toggle-color");         // 1) Set alarm
+    tags.push("expr-fill-color");           // 2) Variable for the fill color
+    tags.push("expr-fill-color-alarm");     
+    tags.push("expr-frame-color");          // 4) Variable for the frame color
+    tags.push("expr-frame-color-alarm");
+    tags.push("expr-invisible");            // 6) Flag to make the object invisible
+    tags.push("expr-fill-flags");           // 7) Toggles the "has-inside-color"
+    tags.push("expr-frame-flags");          // 8) Toggles the "has-frame-color"
+    tags.push("expr-line-width");           // 9) line-width
+
+    // Transition tags
+    tags.push("expr-left");                 // Relative left
+    tags.push("expr-right");                // Relative right
+    tags.push("expr-top");                  // Relative top
+    tags.push("expr-bottom");               // Relative bottom
+    tags.push("expr-xpos");                 // Absolute x-position
+    tags.push("expr-ypos");                 // Absolute y-position
+    tags.push("expr-scale");                // Scale with middle reference point
+
+
+    tags.forEach(function(entry){
+        section.children(entry).children("expr").each(function() {
+        exprMap.set(entry, $(this)!.children("var").text());
+        })
+        
+    });
+
+    return exprMap;
+}
+
+export function parseUserEvent(section : JQuery<XMLDocument>) : string[] {
+    let varList : string[] = [];
+    // Parse the <expr-toggle-var><expr><var> ... elements => toggle color
+    section.children("expr-toggle-var").children("expr").each(function() {
+       varList.push($(this).children("var").text());
+    })
+    return varList;
+}
---
+++ src/pars/Elements/Simpleshape/Features/objectManager.ts
@@ -0,0 +1,111 @@
+import ComSocket from '../../../../com/comsocket';
+import {IVisuObject} from './interfaces'
+import {numberToHexColor} from '../../../Utils/utilfunctions'
+
+export function attachDynamicParameters(visuObject: IVisuObject, dynamicElements : Map<string,string>) : IVisuObject{
+    // Processing the variables for visual elements
+    // 1) Set alarm state
+    if (dynamicElements.has("expr-toggle-color")) {
+        let element = dynamicElements!.get("expr-toggle-color");
+        Object.defineProperty(visuObject, "alarm", {
+            get: function() {
+                let value = ComSocket.singleton().oVisuVariables.get(element)!.value;
+                if (value === "0"){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+    }
+    // 2) Set fill color
+    if (dynamicElements.has("expr-fill-color")) {
+        let element = dynamicElements!.get("expr-fill-color");
+        Object.defineProperty(visuObject, "normalFillColor", {
+            get: function() {
+                return numberToHexColor(ComSocket.singleton().oVisuVariables.get(element)!.value);
+            }
+        });
+    }
+    // 3) Set alarm color
+    if (dynamicElements.has("expr-fill-color-alarm")) {
+        let element = dynamicElements!.get("expr-fill-color-alarm");
+        Object.defineProperty(visuObject, "alarmFillColor", {
+            get: function() {
+                return numberToHexColor(ComSocket.singleton().oVisuVariables.get(element)!.value);
+            }
+        });
+    }
+    // 4) Set frame color
+    if (dynamicElements.has("expr-frame-color")) {
+        let element = dynamicElements!.get("expr-frame-color");
+        Object.defineProperty(visuObject, "normalFrameColor", {
+            get: function() {
+                return numberToHexColor(ComSocket.singleton().oVisuVariables.get(element)!.value);
+            }
+        });
+    }
+    // 5) Set alarm frame color
+    if (dynamicElements.has("expr-frame-color-alarm")) {
+        let element = dynamicElements!.get("expr-frame-color-alarm");
+        Object.defineProperty(visuObject, "alarmFrameColor", {
+            get: function() {
+                return numberToHexColor(ComSocket.singleton().oVisuVariables.get(element)!.value);
+            }
+        });
+    }
+
+    // 6) Set invisible state
+    if (dynamicElements.has("expr-invisible")) {
+        let element = dynamicElements!.get("expr-invisible");
+        Object.defineProperty(visuObject, "display", {
+            get: function() {
+                let value = ComSocket.singleton().oVisuVariables.get(element)!.value;
+                if (value === "0"){
+                    return "visible";
+                } else {
+                    return "hidden";
+                }
+            }
+        });
+    }
+    // 7) Set fill flag state
+    if (dynamicElements.has("expr-fill-flags")) {
+        let element = dynamicElements!.get("expr-fill-flags");
+        Object.defineProperty(visuObject, "hasFillColor", {
+            get: function() {
+                let value = ComSocket.singleton().oVisuVariables.get(element)!.value;
+                if (value === "0"){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+    }
+    // 8) Set frame flag state
+    if (dynamicElements.has("expr-frame-flags")) {
+        let element = dynamicElements!.get("expr-frame-flags");
+        Object.defineProperty(visuObject, "hasFrameColor", {
+            get: function() {
+                let value = ComSocket.singleton().oVisuVariables.get(element)!.value;
+                if (value === "0"){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+    }
+    // 9) line-width
+    if (dynamicElements.has("expr-line-width")) {
+        let element = dynamicElements!.get("expr-line-width");
+        Object.defineProperty(visuObject, "strokeWidth", {
+            get: function() {
+                return Number(ComSocket.singleton().oVisuVariables.get(element)!.value);
+                }
+        });
+    }
+
+    return visuObject;
+}
