25ba48bfc6d23640eff0b044753f796ee5c00bd2
tnentwig <tristan.nentwig@googlemail.com>
2019-10-30 23:18:11 +0100

    New build

--- dist/WebVisu.dev.js
+++ dist/WebVisu.dev.js
@@ -54905,6 +54905,249 @@ if (false) {} else {
 
 /***/ }),
 
+/***/ "./node_modules/sprintf-js/src/sprintf.js":
+/*!************************************************!*\
+  !*** ./node_modules/sprintf-js/src/sprintf.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_RESULT__;/* global window, exports, define */
+
+!function() {
+    'use strict'
+
+    var re = {
+        not_string: /[^s]/,
+        not_bool: /[^t]/,
+        not_type: /[^T]/,
+        not_primitive: /[^v]/,
+        number: /[diefg]/,
+        numeric_arg: /[bcdiefguxX]/,
+        json: /[j]/,
+        not_json: /[^j]/,
+        text: /^[^\x25]+/,
+        modulo: /^\x25{2}/,
+        placeholder: /^\x25(?:([1-9]\d*)\$|\(([^)]+)\))?(\+)?(0|'[^$])?(-)?(\d+)?(?:\.(\d+))?([b-gijostTuvxX])/,
+        key: /^([a-z_][a-z_\d]*)/i,
+        key_access: /^\.([a-z_][a-z_\d]*)/i,
+        index_access: /^\[(\d+)\]/,
+        sign: /^[+-]/
+    }
+
+    function sprintf(key) {
+        // `arguments` is not an array, but should be fine for this call
+        return sprintf_format(sprintf_parse(key), arguments)
+    }
+
+    function vsprintf(fmt, argv) {
+        return sprintf.apply(null, [fmt].concat(argv || []))
+    }
+
+    function sprintf_format(parse_tree, argv) {
+        var cursor = 1, tree_length = parse_tree.length, arg, output = '', i, k, ph, pad, pad_character, pad_length, is_positive, sign
+        for (i = 0; i < tree_length; i++) {
+            if (typeof parse_tree[i] === 'string') {
+                output += parse_tree[i]
+            }
+            else if (typeof parse_tree[i] === 'object') {
+                ph = parse_tree[i] // convenience purposes only
+                if (ph.keys) { // keyword argument
+                    arg = argv[cursor]
+                    for (k = 0; k < ph.keys.length; k++) {
+                        if (arg == undefined) {
+                            throw new Error(sprintf('[sprintf] Cannot access property "%s" of undefined value "%s"', ph.keys[k], ph.keys[k-1]))
+                        }
+                        arg = arg[ph.keys[k]]
+                    }
+                }
+                else if (ph.param_no) { // positional argument (explicit)
+                    arg = argv[ph.param_no]
+                }
+                else { // positional argument (implicit)
+                    arg = argv[cursor++]
+                }
+
+                if (re.not_type.test(ph.type) && re.not_primitive.test(ph.type) && arg instanceof Function) {
+                    arg = arg()
+                }
+
+                if (re.numeric_arg.test(ph.type) && (typeof arg !== 'number' && isNaN(arg))) {
+                    throw new TypeError(sprintf('[sprintf] expecting number but found %T', arg))
+                }
+
+                if (re.number.test(ph.type)) {
+                    is_positive = arg >= 0
+                }
+
+                switch (ph.type) {
+                    case 'b':
+                        arg = parseInt(arg, 10).toString(2)
+                        break
+                    case 'c':
+                        arg = String.fromCharCode(parseInt(arg, 10))
+                        break
+                    case 'd':
+                    case 'i':
+                        arg = parseInt(arg, 10)
+                        break
+                    case 'j':
+                        arg = JSON.stringify(arg, null, ph.width ? parseInt(ph.width) : 0)
+                        break
+                    case 'e':
+                        arg = ph.precision ? parseFloat(arg).toExponential(ph.precision) : parseFloat(arg).toExponential()
+                        break
+                    case 'f':
+                        arg = ph.precision ? parseFloat(arg).toFixed(ph.precision) : parseFloat(arg)
+                        break
+                    case 'g':
+                        arg = ph.precision ? String(Number(arg.toPrecision(ph.precision))) : parseFloat(arg)
+                        break
+                    case 'o':
+                        arg = (parseInt(arg, 10) >>> 0).toString(8)
+                        break
+                    case 's':
+                        arg = String(arg)
+                        arg = (ph.precision ? arg.substring(0, ph.precision) : arg)
+                        break
+                    case 't':
+                        arg = String(!!arg)
+                        arg = (ph.precision ? arg.substring(0, ph.precision) : arg)
+                        break
+                    case 'T':
+                        arg = Object.prototype.toString.call(arg).slice(8, -1).toLowerCase()
+                        arg = (ph.precision ? arg.substring(0, ph.precision) : arg)
+                        break
+                    case 'u':
+                        arg = parseInt(arg, 10) >>> 0
+                        break
+                    case 'v':
+                        arg = arg.valueOf()
+                        arg = (ph.precision ? arg.substring(0, ph.precision) : arg)
+                        break
+                    case 'x':
+                        arg = (parseInt(arg, 10) >>> 0).toString(16)
+                        break
+                    case 'X':
+                        arg = (parseInt(arg, 10) >>> 0).toString(16).toUpperCase()
+                        break
+                }
+                if (re.json.test(ph.type)) {
+                    output += arg
+                }
+                else {
+                    if (re.number.test(ph.type) && (!is_positive || ph.sign)) {
+                        sign = is_positive ? '+' : '-'
+                        arg = arg.toString().replace(re.sign, '')
+                    }
+                    else {
+                        sign = ''
+                    }
+                    pad_character = ph.pad_char ? ph.pad_char === '0' ? '0' : ph.pad_char.charAt(1) : ' '
+                    pad_length = ph.width - (sign + arg).length
+                    pad = ph.width ? (pad_length > 0 ? pad_character.repeat(pad_length) : '') : ''
+                    output += ph.align ? sign + arg + pad : (pad_character === '0' ? sign + pad + arg : pad + sign + arg)
+                }
+            }
+        }
+        return output
+    }
+
+    var sprintf_cache = Object.create(null)
+
+    function sprintf_parse(fmt) {
+        if (sprintf_cache[fmt]) {
+            return sprintf_cache[fmt]
+        }
+
+        var _fmt = fmt, match, parse_tree = [], arg_names = 0
+        while (_fmt) {
+            if ((match = re.text.exec(_fmt)) !== null) {
+                parse_tree.push(match[0])
+            }
+            else if ((match = re.modulo.exec(_fmt)) !== null) {
+                parse_tree.push('%')
+            }
+            else if ((match = re.placeholder.exec(_fmt)) !== null) {
+                if (match[2]) {
+                    arg_names |= 1
+                    var field_list = [], replacement_field = match[2], field_match = []
+                    if ((field_match = re.key.exec(replacement_field)) !== null) {
+                        field_list.push(field_match[1])
+                        while ((replacement_field = replacement_field.substring(field_match[0].length)) !== '') {
+                            if ((field_match = re.key_access.exec(replacement_field)) !== null) {
+                                field_list.push(field_match[1])
+                            }
+                            else if ((field_match = re.index_access.exec(replacement_field)) !== null) {
+                                field_list.push(field_match[1])
+                            }
+                            else {
+                                throw new SyntaxError('[sprintf] failed to parse named argument key')
+                            }
+                        }
+                    }
+                    else {
+                        throw new SyntaxError('[sprintf] failed to parse named argument key')
+                    }
+                    match[2] = field_list
+                }
+                else {
+                    arg_names |= 2
+                }
+                if (arg_names === 3) {
+                    throw new Error('[sprintf] mixing positional and named placeholders is not (yet) supported')
+                }
+
+                parse_tree.push(
+                    {
+                        placeholder: match[0],
+                        param_no:    match[1],
+                        keys:        match[2],
+                        sign:        match[3],
+                        pad_char:    match[4],
+                        align:       match[5],
+                        width:       match[6],
+                        precision:   match[7],
+                        type:        match[8]
+                    }
+                )
+            }
+            else {
+                throw new SyntaxError('[sprintf] unexpected placeholder')
+            }
+            _fmt = _fmt.substring(match[0].length)
+        }
+        return sprintf_cache[fmt] = parse_tree
+    }
+
+    /**
+     * export to either browser or node.js
+     */
+    /* eslint-disable quote-props */
+    if (true) {
+        exports['sprintf'] = sprintf
+        exports['vsprintf'] = vsprintf
+    }
+    if (typeof window !== 'undefined') {
+        window['sprintf'] = sprintf
+        window['vsprintf'] = vsprintf
+
+        if (true) {
+            !(__WEBPACK_AMD_DEFINE_RESULT__ = (function() {
+                return {
+                    'sprintf': sprintf,
+                    'vsprintf': vsprintf
+                }
+            }).call(exports, __webpack_require__, exports, module),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__))
+        }
+    }
+    /* eslint-enable quote-props */
+}(); // eslint-disable-line
+
+
+/***/ }),
+
 /***/ "./node_modules/tiny-warning/dist/tiny-warning.esm.js":
 /*!************************************************************!*\
   !*** ./node_modules/tiny-warning/dist/tiny-warning.esm.js ***!
@@ -55190,7 +55433,7 @@ function parseDynamicTextParameters(section) {
     tags.push("expr-text-flags");
     tags.push("expr-font-flags");
     tags.push("expr-font-name");
-    tags.push("expr-text-display");
+    tags.push("text-display");
     tags.push("expr-text-color");
     tags.push("expr-font-height");
     tags.forEach(function (entry) {
@@ -55408,6 +55651,7 @@ var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var mobx_react_lite_1 = __webpack_require__(/*! mobx-react-lite */ "./node_modules/mobx-react-lite/dist/index.module.js");
 var comsocket_1 = __webpack_require__(/*! ../../../../com/comsocket */ "./src/com/comsocket.ts");
 var utilfunctions_1 = __webpack_require__(/*! ../../../Utils/utilfunctions */ "./src/pars/Utils/utilfunctions.ts");
+var sprintf_js_1 = __webpack_require__(/*! sprintf-js */ "./node_modules/sprintf-js/src/sprintf.js");
 exports.Textfield = function (_a) {
     var section = _a.section, dynamicParameters = _a.dynamicParameters;
     var fontName = (section.children("font-name").text().length) ? section.children("font-name").text() : "Arial";
@@ -55433,14 +55677,15 @@ exports.Textfield = function (_a) {
         hasStrikeOut: hasStrikeOut,
         textAlignHorz: textAlignHorz,
         textAlignVert: textAlignVert,
-        text: text,
+        textStatic: text,
+        textVariable: "",
         textAnchor: "middle",
         xpos: "50%",
         ypos: "50%",
         vertAlign: "middle",
         fontStyle: "normal",
         textDecoration: "initial",
-        output: text
+        textOutput: text
     };
     if (dynamicParameters.has("expr-text-flags")) {
         var element_1 = dynamicParameters.get("expr-text-flags");
@@ -55509,26 +55754,41 @@ exports.Textfield = function (_a) {
             }
         });
     }
-    if (dynamicParameters.has("expr-text-color")) {
-        var element_4 = dynamicParameters.get("expr-text-color");
-        Object.defineProperty(initial, "fontColor", {
+    if (dynamicParameters.has("text-display")) {
+        var element_4 = dynamicParameters.get("text-display");
+        Object.defineProperty(initial, "textVariable", {
             get: function () {
                 var value = comsocket_1.default.singleton().oVisuVariables.get(element_4).value;
+                return value;
+            }
+        });
+    }
+    if (dynamicParameters.has("expr-text-color")) {
+        var element_5 = dynamicParameters.get("expr-text-color");
+        Object.defineProperty(initial, "fontColor", {
+            get: function () {
+                var value = comsocket_1.default.singleton().oVisuVariables.get(element_5).value;
                 var hex = utilfunctions_1.numberToHexColor(value);
                 return hex;
             }
         });
     }
     if (dynamicParameters.has("expr-font-height")) {
-        var element_5 = dynamicParameters.get("expr-font-height");
+        var element_6 = dynamicParameters.get("expr-font-height");
         Object.defineProperty(initial, "fontHeight", {
             get: function () {
-                var value = (-1) * Number(comsocket_1.default.singleton().oVisuVariables.get(element_5).value);
-                value = value / 1.3;
+                var value = (-1) * Number(comsocket_1.default.singleton().oVisuVariables.get(element_6).value);
+                value = value / 1.4;
                 return value;
             }
         });
     }
+    Object.defineProperty(initial, "textOutput", {
+        get: function () {
+            var output = sprintf_js_1.sprintf(initial.textStatic, initial.textVariable);
+            return output;
+        }
+    });
     Object.defineProperty(initial, "textAnchor", {
         get: function () {
             var position = (initial.textAlignHorz === 'center') ? 'middle' : ((initial.textAlignHorz === 'left') ? 'start' : 'end');
@@ -55556,13 +55816,19 @@ exports.Textfield = function (_a) {
             if (initial.hasUnderline) {
                 string = "underline ";
             }
-            return string;
+            if (string.length) {
+                return string;
+            }
+            else {
+                return "initial";
+            }
         }
     });
+    var trued = true;
     var state = mobx_react_lite_1.useLocalStore(function () { return initial; });
     return mobx_react_lite_1.useObserver(function () {
         return React.createElement("text", { textDecoration: state.textDecoration, fontStyle: state.fontStyle, fill: state.fontColor, fontWeight: state.fontWeight, fontSize: -state.fontHeight, fontFamily: state.fontName, textAnchor: state.textAnchor, pointerEvents: 'none' },
-            React.createElement("tspan", { alignmentBaseline: state.vertAlign, x: state.xpos, y: state.ypos }, text));
+            React.createElement("tspan", { alignmentBaseline: "central", x: state.xpos, y: state.ypos }, state.textOutput));
     });
 };
 
@@ -55845,7 +56111,13 @@ function parseSimpleShape(section) {
             enable_text_input: util.stringToBoolean(section.children("enable-text-input").text())
         };
         var dynamicTextParameters = eventParser_1.parseDynamicTextParameters(section);
-        var textField = React.createElement(textManager_1.Textfield, { section: section, dynamicParameters: dynamicTextParameters });
+        var textField = void 0;
+        if (section.find("text-id").text().length) {
+            textField = React.createElement(textManager_1.Textfield, { section: section, dynamicParameters: dynamicTextParameters });
+        }
+        else {
+            textField = null;
+        }
         var dynamicShapeParameters = eventParser_1.parseDynamicShapeParameters(section);
         var userEvents = eventParser_1.parseUserEvent(section);
         switch (shape) {
@@ -56222,7 +56494,7 @@ var HTML5Visu = (function () {
         })
             .then(function (data) {
             _this.initCommunication(data);
-            _this.convertVisuElements(data);
+            setTimeout(function () { return _this.convertVisuElements(data); }, 800);
         })
             .fail(function (error) {
             return console.error(error);
