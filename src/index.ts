c83d90f8fc4ce43ec11533a36e665b4956e0583e
tnentwig <tristan.nentwig@googlemail.com>
2019-11-08 23:04:54 +0100

    Added automatic URI detection  of the location on the uri

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import HTML5Visu from './pars/visuparser';
+import * as React from 'react';
 
-var objHTML5Visu = new HTML5Visu("http://localhost:8080")
-objHTML5Visu.createVisu("/plc_visu.xml");
+if (process.env.NODE_ENV !== 'production') {
+  const {whyDidYouUpdate} = require('why-did-you-update')
+  whyDidYouUpdate(React)
+}
+
+let path = location.protocol + '//' + window.location.host;
+try{
+    const objHTML5Visu = new HTML5Visu(path)
+    objHTML5Visu.createVisu("/plc_visu.xml");
+} catch {
+    console.log("The URI is malformed!");
+}
 
