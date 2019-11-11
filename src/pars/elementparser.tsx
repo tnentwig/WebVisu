09768938d697fb191689ed5d09793ba39b5351b8
tnentwig <tristan.nentwig@googlemail.com>
2019-11-11 15:11:24 +0100

    Separeted visuparser and elementparser

---
+++ src/pars/elementparser.tsx
@@ -0,0 +1,67 @@
+import * as $ from 'jquery';
+import * as React from 'react';
+import { parseSimpleShape } from '../pars/Elements/Basicshapes/simpleshape';
+import { Placeholder } from '../pars/Elements/placeholder';
+import { parsePolyshape } from '../pars/Elements/Basicshapes/polyshape';
+import { parseButton } from '../pars/Elements/Button/button';
+import { Scrollbar } from '../pars/Elements/Scrollbar/scrollbar';
+import { parseArrayTable } from '../pars/Elements/arraytable';
+import { parseBitmap } from '../pars/Elements/bitmap';
+import { Group } from '../pars/Elements/Group/parseGroup'
+
+type Props = {
+    visualisation: JQuery<XMLDocument>
+}
+export const VisuElements :React.FunctionComponent<Props> =({visualisation})=>{
+        
+    console.log("Start parsing...");
+    let visuObjects: Array<(JSX.Element | undefined | null)> =[];
+    // Rip all <element> sections
+    visualisation.children("visualisation").children("element").each(function(){
+        let section = $(this);
+        // Determine the type of the element
+        switch(section.attr("type")) {
+            // Is a simple shape like rectangle, round-rectangle, circle or line
+            case "simple":
+                visuObjects.push(parseSimpleShape(section));
+                break;
+            // Is a bitmap
+            case "bitmap":
+                visuObjects.push(parseBitmap(section));
+                break;
+            // Is a button
+            case "button":
+                visuObjects.push(parseButton(section));
+                break;
+            // Is a polygon - As polygon, polyline or bezier
+            case "polygon":
+                visuObjects.push(parsePolyshape(section))
+                break;
+            // Is a piechart
+            case "piechart":
+                break;
+            // Is a group (Dynamic elements like a graph)
+            case "group":
+                visuObjects.push(<Group section={section}></Group>);
+                break;
+            // Is a Scrollbar
+            case "scrollbar":
+                visuObjects.push(<Scrollbar section={section}></Scrollbar>);
+                break;
+            // Not a supported type, is logged
+            case "array-table":
+                visuObjects.push(parseArrayTable(section));
+                break;
+            default:
+                console.log("Type <"+section.attr("type")+"> is not supported yet!");
+        }
+    });
+    
+    return (
+        <React.Fragment>
+            {
+                visuObjects.map((element, index)=><React.Fragment>{element}</React.Fragment>)
+            }
+        </React.Fragment>
+    )
+}
