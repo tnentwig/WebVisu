a7db56b8745861aa41659c7f28b867f35dd72cea
tnentwig <tristan.nentwig@googlemail.com>
2019-11-18 17:46:35 +0100

    Bug fixed width and height

--- src/visu/pars/Elements/Basicshapes/SimpleSubunits/roundrect.tsx
+++ src/visu/pars/Elements/Basicshapes/SimpleSubunits/roundrect.tsx
@@ -22,7 +22,7 @@ export const Roundrect :React.FunctionComponent<Props> = ({simpleShape, textFiel
     const state  = useLocalStore(()=>initial);
     
     return useObserver(()=>
-    <div id={simpleShape.elem_id} style={{cursor: "auto",overflow:"hidden", pointerEvents: state.eventType, visibility : state.display, position:"absolute", left:state.transformedCornerCoord.x1-state.edge, top:state.transformedCornerCoord.y1-state.edge, width:state.relCoord.width+state.edge, height:state.relCoord.height+state.edge}}>
+    <div id={simpleShape.elem_id} style={{cursor: "auto",overflow:"hidden", pointerEvents: state.eventType, visibility : state.display, position:"absolute", left:state.transformedCornerCoord.x1-state.edge, top:state.transformedCornerCoord.y1-state.edge, width:state.relCoord.width+2*state.edge, height:state.relCoord.height+2*state.edge}}>
         {input}
         <svg>
             <svg 
