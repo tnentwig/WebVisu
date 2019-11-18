245c94ceb418d638680af0fff1a5f4f6cee4497c
tnentwig <tristan.nentwig@googlemail.com>
2019-11-18 17:41:35 +0100

    Created controlcenter the currentvisu and cycletime is managed

--- src/controlcenter.tsx
+++ src/controlcenter.tsx
@@ -6,49 +6,111 @@ import {Visualisation} from './visu/visuparser'
 
 export default class HTML5Visu {
     rootDir: string;
-    constructor(rootDir: string){
-        this.rootDir= rootDir;
+    constructor(){
+        let path = location.protocol + '//' + window.location.host;
+        this.rootDir= path;
     }
-    createVisu (relPath: string) {
-        $.ajax({
+    async showMainVisu () {
+         // Get the webvisu.htm file. There are the startvisu and updatetime listed
+        let visuProp : Map<string,string>;
+        try{
+           // Get the webvisu.htm file. There are the startvisu and updatetime listed
+           visuProp = await this.getWebvisuhtm('/webvisu.htm');
+        } catch {
+           throw new Error("The URI is malformed!");
+        }
+       
+        // Get the visu-ini file. There are informations like the current user level, the current visu or the user passwords
+        let visuIni = await this.getVisuini('/visu_ini.xml');
+        // The Comsocket has to be initilized
+        await this.initCommunication(visuIni, 200);
+        // Get the current visu
+        let currentVisu = visuProp.get("STARTVISU");
+        this.displayVisu(currentVisu);
+    }
+
+    async initCommunication(XML : XMLDocument, cycletime : number) : Promise<boolean> {
+        return new Promise(resolve =>{
+            let com = ComSocket.singleton();
+            com.setServerURL(this.rootDir + '');
+            com.startCyclicUpdate(200);
+            this.appendVariables(XML);
+            resolve(true);
+        })
+    }
+
+    appendVariables(XML : XMLDocument) : Promise<boolean>{
+        return new Promise(resolve => {
+            let visuXML=$(XML);
+            // Rip all of <variable> in <variablelist> section
+            visuXML.children("visu-ini-file").children("variablelist").children("variable").each(function(){
+                let variable = $(this);
+                ComSocket.singleton().addObservableVar(variable.attr("name"), variable.text());
+            });
+        })
+    }
+
+    getWebvisuhtm(relPath : string) : Promise<Map<string,string>>{
+    return new Promise((resolve)=>{
+        let map :Map<string,string> = new Map();    
+    // Get the webvisu.htm file. There are the startvisu and updatetime listed
+        let webvisuhtm =$.ajax({
             url: this.rootDir+relPath,
             type: 'GET',
-            dataType: 'XML', // if text => no pre-processing, if xml => parseXML preprocessing
+            dataType: 'html', 
             crossDomain: true
         })
 
-        .then((data) => {
-            // Searching for variables and initialize the communication
-            this.initCommunication(data);
-            // Searching for elements
-            setTimeout(()=>this.displayVisu(data), 800);
+        webvisuhtm.then((data) => {
+            // Searching for cycletime and startvisuname
+            let parser = new DOMParser();
+            let htmlDoc = parser.parseFromString(data, 'text/html');
+            let htmlElement = htmlDoc.getElementsByTagName("param");
+            let visuName = "";
+            let updateTime = "";
+            for (let i=0; i<htmlElement.length; i++){
+                let name = htmlElement[i].getAttribute("name").toString();
+                switch(name){
+                    case "STARTVISU":
+                        visuName = htmlElement[i].getAttribute("value");
+                        console.log(visuName);
+                        map.set(name, visuName)
+                        break;
+                    case "UPDATETIME":
+                        updateTime = htmlElement[i].getAttribute("value");
+                        map.set(name, updateTime)
+                }
             }
-        )
+            resolve(map)
+        })       
         .fail((error) => {
             console.error(error);
         })
+    })
     }
 
-    initCommunication(XML : XMLDocument) {
-        let com = ComSocket.singleton();
-        com.setServerURL(this.rootDir + '');
-        let visuXML=$(XML);
-        // Rip all of <variable> in <variablelist> section
-        visuXML.children("visualisation").children("variablelist").children("variable").each(function(){
-            let variable = $(this);
-            com.addObservableVar(variable.attr("name"), variable.text());
-        });
-        com.updateVarList();
-        com.startCyclicUpdate(200);
+    getVisuini(relPath : string) : Promise<XMLDocument>{
+        return new Promise(resolve=>{
+            $.ajax({
+                url: this.rootDir+relPath,
+                type: 'GET',
+                dataType: 'xml', // if text => no pre-processing, if xml => parseXML preprocessing
+                crossDomain: true
+            })
+            .then((data)=>resolve(data))
+            .fail((error) => {
+                console.error(error);
+            })
+        })
     }
 
-    displayVisu (XML : XMLDocument)  {
+    displayVisu (name : string)  {
         // The coverted sections are inserted in the virtual react DOM
         function App() {
             return (
                 <React.Fragment>
                     {
-                        <Visualisation visualisation={XML}></Visualisation>
+                        <Visualisation visuname={name}></Visualisation>
                     }
                 </React.Fragment>
             )
