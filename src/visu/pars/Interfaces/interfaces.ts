bcf70cfe2afd3399508694946cb8ef27b9b2c957
tnentwig <tristan.nentwig@googlemail.com>
2019-11-25 20:44:19 +0100

    Merged all interfaces to one file

--- src/visu/pars/Interfaces/interfaces.ts
+++ src/visu/pars/Interfaces/interfaces.ts
@@ -28,3 +28,41 @@ export interface IComSocket {
     startCyclicUpdate(periodms : number) : void;
     toggleValue(varName : string) : void;
 }
+
+export interface IVisuObject{
+    absCornerCoord : {x1:number,y1:number,x2:number,y2:number},
+    absCenterCoord : {x:number, y:number},
+    transformedCornerCoord : {x1:number,y1:number,x2:number,y2:number},
+    relCoord : {width:number,height:number}
+    relMidpointCoord : {x:number,y:number}
+    left : number,
+    right : number,
+    top : number,
+    bottom : number,
+    xpos : number,
+    ypos : number,
+    edge : number,
+    scale : number,
+    angle : number,
+    normalFillColor : string,
+    alarmFillColor : string,
+    normalFrameColor : string,
+    alarmFrameColor : string,
+    hasFillColor : boolean,
+    hasFrameColor : boolean,
+    lineWidth : number,
+    strokeWidth : number,
+    display : any,
+    tooltip : string,
+    eventType : any,
+    alarm : boolean,
+    fill : string,
+    stroke : string,
+    strokeDashArray : string,
+    // The following variables are for polyshapes only
+    absPoints : number[][],
+    relPoints : number[][],
+    cssTransform : string,
+    cssTransformOrigin : string
+
+}
