9fde50c262e66ad9b3cf20292ccd8a9cc09bd682
tnentwig <tristan.nentwig@googlemail.com>
2020-01-26 21:06:00 +0100

    Changed type of dynamicParameters to Array

--- src/visu/pars/Elements/Basicshapes/SimpleSubunits/rectangle.tsx
+++ src/visu/pars/Elements/Basicshapes/SimpleSubunits/rectangle.tsx
@@ -7,7 +7,7 @@ type Props = {
     simpleShape: IBasicShape,
     textField : JSX.Element|undefined,
     input : JSX.Element,
-    dynamicParameters :  Map<string,{type:string, value:string, arithmetic:string}>,
+    dynamicParameters :  Map<string,string[][]>,
     onmousedown : Function,
     onmouseup : Function,
     onclick : Function 
