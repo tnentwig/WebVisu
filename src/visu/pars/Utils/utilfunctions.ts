f1d7f34f37548507b315c4570e23a293beedf031
tnentwig <tristan.nentwig@googlemail.com>
2020-02-02 22:33:16 +0100

    Added functions for piechart element

--- src/visu/pars/Utils/utilfunctions.ts
+++ src/visu/pars/Utils/utilfunctions.ts
@@ -52,6 +52,68 @@ export function computeMinMaxCoord(pointArray : number[][]): number[] {
     return rect;
 }
 
+export function computePiechartRectCoord(pointArray : number[][]): number[] {
+    let deltax = Math.abs(pointArray[1][0] - pointArray[0][0]);
+    let deltay = Math.abs(pointArray[1][1] - pointArray[0][1]);
+    let x1 = pointArray[0][0]-deltax;
+    let x2 = pointArray[0][0]+deltax;
+    let y1 = pointArray[0][1]-deltay;
+    let y2 = pointArray[0][1]+deltay;
+    let rect = [x1, y1, x2, y2]
+    return rect;
+}
+
+// Calculate the radius of a specifc point
+function radius(a : number, b: number, angle : number){
+    let r = a*b/(Math.sqrt(Math.pow(b*Math.cos(angle), 2)+Math.pow(a*Math.sin(angle), 2)));
+    return r;
+}
+
+export function pointArrayToPiechartString(pointArray : number[][], startAngleDegree : number, endAngleDegree : number, strokeWidth: number) : string{
+    // Calculate the angle in radiant
+    let degreeToRad = 2*Math.PI/360;
+    let startAngleRad = -startAngleDegree *degreeToRad;
+    let endAngleRad = -endAngleDegree *degreeToRad;
+    // Calculate the radii of the ellipse
+    let radiusx = Math.abs(pointArray[1][0]-1 - pointArray[0][0]);
+    let radiusy = Math.abs(pointArray[1][1]-1 - pointArray[0][1]);
+    // Calculate the radius of start and endpoint
+
+    let rStart = radius(radiusx, radiusy, startAngleRad);
+    let rEnd = radius(radiusx, radiusy, endAngleRad);
+    let start = [Math.cos(startAngleRad)*rStart+radiusx, Math.sin(startAngleRad)*rStart+radiusy];
+    let end = [Math.cos(endAngleRad)*rEnd+radiusx, Math.sin(endAngleRad)*rEnd+radiusy];
+    let interimArray = pointArray;
+    interimArray[2]= start;
+    interimArray[3]= end;
+
+    // Claculate the largeArcFlag
+    let angleDiff = endAngleDegree - startAngleDegree;
+    let largeArcFlag = 1;
+    if (angleDiff > 0){
+        largeArcFlag = endAngleDegree - startAngleDegree <= 180 ? 1 : 0;
+    } else {
+        largeArcFlag = endAngleDegree - startAngleDegree <= 180 ? 0 : 1;
+    }
+    
+
+    var d = [
+        // Move to the center of the div element
+        "M", interimArray[0][0], interimArray[0][1],
+        "L", interimArray[2][0]+strokeWidth, interimArray[2][1]+strokeWidth, 
+        "A", radiusx, radiusy, 0, largeArcFlag, 1, interimArray[3][0]+strokeWidth, interimArray[3][1]+strokeWidth,
+        "Z"
+     ].join(" ");
+    return d;      
+}
+export function polarToCartesian(center : number[], radius : number, angleInDegrees : number) {
+    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+    return {
+      x: center[0] + (radius * Math.cos(angleInRadians)),
+      y: center[1] + (radius * Math.sin(angleInRadians))
+    };
+  }
+
 export function coordArrayToString(pointArray : number[][]) :string {
     let interim : string[] = [];
     pointArray.forEach((element)=>interim.push(element.join(',')));
