758908c3a6e777f4f6245084191ccfaa35e54117
tnentwig <tristan.nentwig@googlemail.com>
2020-03-19 19:35:59 +0100

    Added handling of placeholder replacement

--- src/visu/pars/Elements/Subvisu/subvisu.tsx
+++ src/visu/pars/Elements/Subvisu/subvisu.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as $ from 'jquery';
 import {useObserver } from 'mobx-react-lite';
 import {stringToArray } from '../../Utils/utilfunctions';
 import {Visualisation} from '../../../visuparser'
@@ -15,11 +16,19 @@ export const Subvisu :React.FunctionComponent<Props> = ({section})=>
     let tooltip = (section.children("tooltip").text()).length>0? section.children("tooltip").text() : ""
     // Auxiliary values
     let relCornerCoord = {x1:0, y1:0, x2:rect[2]-rect[0], y2:rect[3]-rect[1]};
+    // Parsing of the placeholder variables
+    let placeholders : Map<string,string> = new Map();
+    section.children("placeholderentry").each(function (){
+        let variable = $(this);
+        let placeholder = variable.attr("placeholder");
+        let replacement = variable.attr("replacement");
+        placeholders.set(placeholder.toLowerCase(), replacement)
+    })
 
     // Return of the react node
     return useObserver(()=>
         <div title={name} style={{position:"absolute", left:rect[0], top:rect[1], width:relCornerCoord.x2, height:relCornerCoord.y2}}>
-            <Visualisation visuname={name.toLowerCase()}></Visualisation>
+            <Visualisation visuname={name.toLowerCase()} mainVisu={false} replacementSet={placeholders}></Visualisation>
         </div>
     )
 }
