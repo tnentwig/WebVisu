86d0169413aa745a1cdbdc22b777df8d310f7181
tnentwig <tristan.nentwig@googlemail.com>
2020-03-19 19:27:20 +0100

    Added possibility to reset the ComSocket Prevents accumulation of variables when changing visus

--- src/visu/communication/comsocket.ts
+++ src/visu/communication/comsocket.ts
@@ -8,15 +8,18 @@ export default class ComSocket implements IComSocket {
     // objList contains all variables as objects with the name as key and addr & value of the variable
     oVisuVariables: Map<string,{addr: string, value: string | undefined}>;
     // The objList has no clear sequence. To get the possibily of correct access with numbers as indices use the look up table
+    private globalVariables: Map<string,{addr: string, key: string}>;
+    // the actual list and map
     private lutKeyVariable: Array<string>;
-    private requestFrame: {preFrame: String, listings: number}
+    private requestFrame: {frame: String, listings: number}
     private serverURL : string;
 
     // this class shall be a singleton
     private constructor() {
         this.serverURL = '';
         this.oVisuVariables  = observable(new Map());
-        this.requestFrame = {preFrame:'', listings:0};
+        this.globalVariables = new Map();
+        this.requestFrame = {frame:'', listings:0};
         this.lutKeyVariable = [];
     }
 
@@ -28,37 +31,56 @@ export default class ComSocket implements IComSocket {
         this.serverURL = serverURL;
     }
 
+    addGlobalVar(varName : string , varAddr : string){
+       this.globalVariables.set(varName,{addr : varAddr, key:varName})
+    }
+
     addObservableVar(varName : string , varAddr : string){
         // Add new variable as object to objList
         if (typeof(varName)==='string') {
             this.oVisuVariables.set(varName,{
                 addr    : varAddr,
-                value   : undefined       // value is undefined at first 
+                value   : ""       // value is undefined at first 
             })
         // Add new variable to the request frame
-        this.requestFrame.preFrame += this.requestFrame.listings + '|' + varAddr.replace(/,/g, '|') + '|';
+        this.requestFrame.frame += this.requestFrame.listings + '|' + varAddr.replace(/,/g, '|') + '|';
         this.requestFrame.listings += 1;
         // Create reference of the key in the LUT 
         this.lutKeyVariable.push(varName);
         }
     }
+
+    initObservables(){
+        // Reset the requestFrame
+        this.requestFrame.frame = "";
+        this.requestFrame.listings = 0;
+        this.lutKeyVariable = [];
+        // Reset the observables Map
+        this.oVisuVariables = observable(new Map());
+        this.globalVariables.forEach((keyValue)=>{
+            this.addObservableVar(keyValue.key, keyValue.addr);
+        })
+    }
+
     updateVarList() {
         try{
         $.ajax({
             type: 'POST',
             contentType: "text/plain",
             url: this.serverURL,
-            data: '|0|'+this.requestFrame.listings+'|'+this.requestFrame.preFrame,
+            data: '|0|'+this.requestFrame.listings+'|'+this.requestFrame.frame,
         })
         .then((response : string) => {
             let transferarray : Array<string>= (response.slice(1,response.length-1).split('|'));
-            for(let i=0; i<transferarray.length; i++) {
-                let varName = this.lutKeyVariable[i];
-                if (this.oVisuVariables.get(varName).value!==transferarray[i]){
-                    action(this.oVisuVariables.get(varName)!.value=transferarray[i]);   
-                }
-            };
-            StateManager.singleton().oState.set("ISONLINE", "TRUE");
+            if (transferarray.length === this.requestFrame.listings){
+                for(let i=0; i<transferarray.length; i++) {
+                    let varName = this.lutKeyVariable[i];
+                    if (this.oVisuVariables.get(varName).value!==transferarray[i]){
+                        action(this.oVisuVariables.get(varName)!.value=transferarray[i]);   
+                    }
+                };
+                StateManager.singleton().oState.set("ISONLINE", "TRUE");
+            }
         })
 
         .fail(()=>{
