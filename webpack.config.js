7b36498c37038df8a8c978bcfd26b1524ae9b3e7
tnentwig <tristan.nentwig@googlemail.com>
2020-04-02 20:15:04 +0200

    Changed to development

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     devtool: 'source-map',
     entry:  './src/index.ts',
     output: {
