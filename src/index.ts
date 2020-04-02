d84e54b6318b1106284c2f8d5294925ce11d1342
tnentwig <tristan.nentwig@googlemail.com>
2020-04-02 20:15:35 +0200

    Removed why-did-you-update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 import HTML5Visu from './controlcenter';
-import * as React from 'react';
-
-if (process.env.NODE_ENV !== 'production') {
-  const {whyDidYouUpdate} = require('why-did-you-update')
-  whyDidYouUpdate(React)
-}
 
 const objHTML5Visu = new HTML5Visu();
 objHTML5Visu.showMainVisu();
