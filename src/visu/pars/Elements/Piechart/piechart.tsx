7dec1d5d5d214673b62dbd3f6777e1a4f5aca3f9
tnentwig <tristan.nentwig@googlemail.com>
2020-04-16 13:10:51 +0200

    Changed path

--- src/visu/pars/Elements/Piechart/piechart.tsx
+++ src/visu/pars/Elements/Piechart/piechart.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as $ from 'jquery';
 import * as util from '../../Utils/utilfunctions';
-import { IBasicShape, IPiechart, IVisuObject } from '../../Interfaces/interfaces';
+import { IBasicShape, IPiechart, IVisuObject } from '../../../Interfaces/interfaces';
 import { Textfield } from '../Features/Text/textManager';
 import { Inputfield } from '../Features/inputManager';
 import { parseDynamicShapeParameters, parseDynamicTextParameters, parseClickEvent ,parseTapEvent} from '../Features/eventManager';
