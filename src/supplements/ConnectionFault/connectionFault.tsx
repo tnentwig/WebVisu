19f50ad1e60874299d36ec9b63e8a9231237dbe3
tnentwig <tristan.nentwig@googlemail.com>
2020-05-11 18:59:01 +0200

    Changed folder

--- src/supplements/ConnectionFault/connectionFault.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export const ConnectionFault : React.FunctionComponent = ()=> {
-    return (
-    <div className="conn-fault">
-        <div>The PLC webserver is not reachable!</div>
-    </div>
-    )
-}
