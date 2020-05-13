a42742b2ea052ac76fa1cb9417cb1b4d37ed6895
tnentwig <tristan.nentwig@googlemail.com>
2020-05-13 23:05:24 +0200

    Subvisu variables are now stored in state - this prevent multiple store calculations on each rerender

--- src/visu/pars/Elements/Subvisu/subvisu.tsx
+++ src/visu/pars/Elements/Subvisu/subvisu.tsx
@@ -8,16 +8,7 @@ type Props = {
     section : JQuery<XMLDocument>
 }
 
-export const Subvisu :React.FunctionComponent<Props> = ({section})=>
-{
-    // Parsing of the fixed parameters
-    let rect = stringToArray(section.children("rect").text());
-    let width = rect[2]-rect[0];
-    let name = section.children("name").text();
-    let tooltip = (section.children("tooltip").text()).length>0? section.children("tooltip").text() : ""
-    // Auxiliary values
-    let relCornerCoord = {x1:0, y1:0, x2:rect[2]-rect[0], y2:rect[3]-rect[1]};
-    // Parsing of the placeholder variables
+function getPlaceholders(section : JQuery<XMLDocument>){
     let placeholders : Map<string,string> = new Map();
     section.children("placeholderentry").each(function (){
         let variable = $(this);
@@ -25,11 +16,19 @@ export const Subvisu :React.FunctionComponent<Props> = ({section})=>
         let replacement = variable.attr("replacement");
         placeholders.set(placeholder.toLowerCase(), replacement.toLowerCase())
     })
+    return placeholders;
+}
 
+export const Subvisu :React.FunctionComponent<Props> = ({section})=>
+{
+    const [visuname, setVisuname] = React.useState(section.children("name").text().toLowerCase());
+    const [rect, setRect] = React.useState(stringToArray(section.children("rect").text()));
+    const [placeholders, setPlaceholders] = React.useState(getPlaceholders(section));
+   
     // Return of the react node
     return useObserver(()=>
-        <div title={name} style={{position:"absolute", left:rect[0], top:rect[1], width:relCornerCoord.x2, height:relCornerCoord.y2}}>
-            <Visualisation visuname={name.toLowerCase()} mainVisu={false} replacementSet={placeholders} width={width}></Visualisation>
+        <div title={name} style={{position:"absolute", left:rect[0], top:rect[1], width:rect[2]-rect[0], height:rect[3]-rect[1]}}>
+            <Visualisation visuname={visuname} mainVisu={false} replacementSet={placeholders} width={rect[2]-rect[0]}></Visualisation>
         </div>
     )
 }
