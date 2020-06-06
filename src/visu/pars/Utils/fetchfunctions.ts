19174f018a3c2273a08298e1f9d501f42be5c593
tnentwig <tristan.nentwig@googlemail.com>
2020-06-06 23:56:58 +0200

    Added getImage A function to get the needed pictures and resolving as datauri

--- src/visu/pars/Utils/fetchfunctions.ts
+++ src/visu/pars/Utils/fetchfunctions.ts
@@ -1,4 +1,5 @@
 import * as JsZip from 'jszip';
+import { get, set } from 'idb-keyval';
 
 export function getVisuxml(url : string) :Promise<XMLDocument> {
     return new Promise(resolve =>{
@@ -13,7 +14,6 @@ export function getVisuxml(url : string) :Promise<XMLDocument> {
                     let data = new window.DOMParser().parseFromString(text, "text/xml")
                     resolve(data)
                 })
-                
             }
             // Try to fetch the visu as zipped file
             else {
@@ -55,4 +55,66 @@ export function parseVisuXML(stringXML : string) : XMLDocument{
     let parser = new DOMParser();
     let returnXML = parser.parseFromString(stringXML, "application/xml");
     return returnXML;
+}
+
+export function getImage(url : string) :Promise<string> {
+    // Calculate the mimeType
+    let mimeType = "";
+    let fileFormat = url.split(".").pop();
+    switch(fileFormat){
+        case "bmp":
+            mimeType = "image/bmp";
+            break;
+        case "jpeg":
+            mimeType = "image/jpeg";
+            break;
+        case "jpg":
+            mimeType = "image/jpeg";
+            break;
+    }
+
+    return new Promise(resolve =>{
+        let base64Flag = 'data:'+mimeType+';base64,'
+        fetch(url)
+        .then((response)=>{
+            // Try to fetch the xml as unzipped file
+            if (response.ok){
+                response.arrayBuffer()
+                .then((buffer)=>{
+                    let binary = '';
+                    let bytes = new Uint8Array(buffer);
+                    bytes.forEach((b) => binary += String.fromCharCode(b));
+                    let base64 = window.btoa(binary);
+                    resolve(base64Flag +base64)
+                })
+            }
+            // Try to fetch the visu as zipped file
+            else {
+                let zip = new JsZip();
+                let urlStack = url.split('/');
+                let filename = urlStack.pop();
+                let zipName = filename.split(".")[0]+"_"+fileFormat+".zip"
+                // Push the zip filename to stack
+                urlStack.push(zipName);
+                fetch(urlStack.join('/'))
+                .then((response)=>{
+                    // Try to fetch the xml as unzipped file
+                    if (response.ok){
+                        response.arrayBuffer()
+                        .then((buffer)=>zip.loadAsync(buffer))
+                        .then((unzipped) => unzipped.file(filename).async("arraybuffer"))
+                        .then((buffer)=>{
+                            let binary = '';
+                            let bytes = new Uint8Array(buffer);
+                            bytes.forEach((b) => binary += String.fromCharCode(b));
+                            let base64 = window.btoa(binary);
+                            resolve(base64Flag +base64)
+                        })
+                    } else {
+                        resolve(null)
+                    }
+                })
+            }
+        })
+    })
 }
