c949e7dafda7406d12e4844d105835f6a5774d9d
Tristan Nentwig <tristan.nentwig@googlemail.com>
2021-02-10 17:23:03 +0100

    Reworked the loader for different controller

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,9 +1,17 @@
 import { decompressSync, strFromU8 } from 'fflate';
 
-fetch('/webvisu/WebVisuApp.js.gz')
-    .then((response) => response.arrayBuffer())
-    .then((data) => {
-        const dataArray = decompressSync(new Uint8Array(data));
-        const WebVisuAppScript = strFromU8(dataArray);
-        eval(WebVisuAppScript);
+const prefixList: Array<String> = ['', '/PLC', '/webvisu'];
+
+prefixList.forEach((prefix) => {
+    fetch(prefix + '/WebVisuApp.js.gz').then((response) => {
+        if (response.ok) {
+            response.arrayBuffer().then((data) => {
+                const dataArray = decompressSync(
+                    new Uint8Array(data),
+                );
+                const WebVisuAppScript = strFromU8(dataArray);
+                eval(WebVisuAppScript);
+            });
+        }
     });
+});
